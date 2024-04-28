cda4baedf0a0dadffa7f0a9b15d273d75bc7c651
Mishal <mishal0404@gmail.com>
2024-04-28 20:57:10 +0530

    finished

--- server.py
+++ server.py
@@ -10,7 +10,14 @@ app = FastAPI()
 
 client = openai.OpenAI()
 
-vectorsestoreid = "vs_TmTo1l9o3hoAhLkWm3EY87T4"
+vectorsestoreid = "vs_oyCadoQAfTDgr6r1OwLbiZPr"
+
+vectorstorepdfs = {'file-5IED2QKjBtQPncyo29sb3bd2':'Home Maintenance For Dummies',
+ 'file-DNbDePyPmU1C9ZhLe56QWAdp':'Packet of Home Maintenance Information',
+ 'file-TYvB0JEnYUSigMhDKeWE6kUy':'Homeowner Maintenance Manual - National Home Warranty',
+ 'file-VNdRZp7AmPBZm8iBHcdKeREJ':'Homeowner Maintenance Manual - StrucSure Home Warranty',
+ 'file-MH3jm6gmvedNnx5TwwTdYrza':'Home Maintenance Student Manual - Northwest Territories Housing Corporation'}
+
 @app.post("/ask_question/")
 
 
@@ -22,7 +29,7 @@ async def ask_question(question: str = Form(...)):
   tools=[{"type": "file_search"}],
   tool_resources={
     "file_search": {
-      "vector_store_ids": ["vs_TmTo1l9o3hoAhLkWm3EY87T4"]
+      "vector_store_ids": [vectorsestoreid]
     }
   }
   ) 
@@ -31,7 +38,7 @@ async def ask_question(question: str = Form(...)):
   run = client.beta.threads.runs.create_and_poll(
   thread_id=thread.id,
   assistant_id=assistant.id,
-  instructions="You are a helpful housing support assistant and you answer questions based only  on the files provided to you. "+ question,
+  instructions="You are a helpful housing support assistant. Answer the following question based only on the files provided to you: "+ question,
   )
 
   if run.status == 'completed': 
@@ -40,10 +47,20 @@ async def ask_question(question: str = Form(...)):
           thread_id=thread.id
       )
       
-      contents = messages.data[0].content
-      print(contents)
-
-      return contents
+      content = messages.data[0].content[0].text
+      print(content)
+      annotations = content.annotations
+      value = content.value
+      filesaccessed = set()
+      for annot in annotations:
+        file_id = annot.file_citation.file_id
+        filename = vectorstorepdfs[file_id]
+        filesaccessed.add(filename)
+      pdffiles = []
+      for file in filesaccessed:
+         pdffiles.append({"name":file})
+      
+      return {"answer":value, "files":pdffiles}
 
         
 
